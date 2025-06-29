7c05eea59c074c5aba4ba543dc01af9c94351c7a
SergeiPyatkov <sergeipyatkov10@gmail.com>
2025-06-29 23:08:56 +0700

    Add files via upload

---
+++ hw94/script/main.js
@@ -0,0 +1,16 @@
+let userName='Сегрей';
+let userSurname='Пятков'
+let userAge='20'
+let userPaul='Мужской'
+let userMood='Good'
+
+console.log('Имя:',userName);
+console.log('Фамилия:',userSurname);
+console.log('Возраст:',userAge);
+console.log('Пол:',userPaul);
+console.log('Настроение:',userMood);
+
+
+
+
+
